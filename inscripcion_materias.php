47e5dd3b68291202855453beff1145c8455553d3
ericlette <37308233+ericlette@users.noreply.github.com>
2018-03-20 22:32:33 -0300

    Update inscripcion_materias.php

--- inscripcion_materias.php
+++ inscripcion_materias.php
@@ -1,126 +1,204 @@
 ﻿<!DOCTYPE html>
-<html>
-<head>
+	<html>
+	<head>
 	<meta charset="utf-8">
-	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" >
-	<title>Inscripcion a Materias</title>
-	<style type="text/css">
-	  /*.normal {
-	    width: 1024px;
-	    border: 1px solid #000;
-	  }
-	  .normal th, .normal td {
-	    border: 1px solid #000;
-	  }*/
-	</style>
-</head>
-<body style="background-color:#EDECEA;">
+
+
+	    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+ 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" >
+
+		<link rel="stylesheet" type="text/css" href="image/fonts/style.css">
+
+<style type="text/css">
+	
+	a{
+  color:white;
+}
+</style>
+
+
+
+
+		<title>Inscripcion a Materias</title>
+
+
+
+	</head>
+	<body>
+
 	<nav style="background-color:#0D0C0C" class="navbar navbar-dark  fixed-top    navbar-toggleable-md ">
-		<button class="navbar-toggler navbar-toggler-right " type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" id="btn-menu" aria-label="Toggle navigation">
-	  	<span class="navbar-toggler-icon"></span>
+
+  <button class="navbar-toggler navbar-toggler-right " type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"  id="btn-menu" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
 
-	  <a class="navbar-brand" href="pagina_principal.php">
-	    <span class="icon-brand35" ></span>
-	    Profesorado CEFN5
-	  </a>
 
- 		<div class="collapse  navbar-collapse" id="menu">
-  		<div class="navbar-nav">
-	      <a class="nav-item nav-link" href="#">Inscripcion Materias</a>
-	      <a class="nav-item nav-link" href="#">Inscripcion Final</a>
-	      <a class="nav-item nav-link" href="#">Ver plan de estudios</a>
-	      <a class="nav-item nav-link" href="#">Obtener crtificados</a>
-    	</div>
+  <a class="navbar-brand" href="login_exitoso.php">
+    <span class="icon-brand35" ></span>
+    rofesorado CEFN5
+  </a>
+
+ <div class="collapse  navbar-collapse" id="menu">
+  <div class="navbar-nav">
+      
+      <a class="nav-item nav-link" href="#">Inscripcion Materias</a>
+      <a class="nav-item nav-link" href="#">Inscripcion Final</a>
+      <a class="nav-item nav-link" href="#">Ver plan de estudios</a>
+      <a class="nav-item nav-link" href="#">Obtener crtificados</a>
+      
+   
     </div>
-	</nav>
+    </div>
+</nav>
 
-	<div class="container">
-		<br>
-		<br>
-		<br>
+	<div class="container-fluid">
 
-		<table class="table" style="background-color:white;">
-	  	<thead class="thead-dark">
-				<tr>
- 					<th scope="col">#NRO</th>
-		      <th scope="col">NOMBRE MATERIA</th>
-		      <th scope="col">AÑO</th>
-		      <th scope="col">FORMATO</th>
-		      <th scope="col">REGIMEN</th>
-		      <th scope="col">INSCRIBIR</th>
-				</tr>
-	   	</thead>
- 			<tbody>
 
-  		<?php
-    	$id_alumno = 1;
+	<br>
+	<br>
+	<br>
 
-	    $server = "localhost";
-	    $usuario = "root";
-	    $contra = "MyNewPass";
-	    $basedato = "profesorado_cef";
+<div class="row justify-content-center ">
+    
 
-	    try {
-	      $con = new PDO('mysql:host='.$server.';dbname='.$basedato.'', "$usuario", "$contra");
-	      $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	 
+	<table class="table">
+	  <thead class="thead-dark">
+		<tr>
+		
 
-	      $sql = $con->prepare(
-	        'SELECT c.id_catedra,c.id_profesor,c.nombre,c.ano,c.formato,c.regimen_c, p.nombre_y_apellido FROM catedras AS c INNER JOIN profesores AS p ON c.id_profesor = p.id_profesor'
-	      );
-	      $sql->execute();
+			 <th scope="col">#NRO</th>
+	      
+	      <th scope="col">NOMBRE MATERIA</th>
+	      <th scope="col">AÑO</th>
+	       <th scope="col">FORMATO</th>
+	      <th scope="col">REGIMEN</th>
+	      <th scope="col">INSCRIBIR</th>
+	      
 
-	      $cadena = "";
-	      $cadena.="<tr>";
 
-	      while($datos = $sql->fetch(PDO::FETCH_ASSOC)){
-	      	$cadena.="<td>".$datos['id_catedra']."</td>
-	      	 <td>".$datos['nombre']."</td>
-	      	 <td>".$datos['ano']."</td>
-	      	 <td>".$datos['formato']."</td>
-	      	 <td>".$datos['regimen_c']."</td>
-	      	 <td><button class='btn btn-outline-primary' onclick='inscribir(".$id_alumno.",".$datos['id_catedra'].")'>Aquí</button></td>";
+		</tr>
+	   </thead>
+	   <tbody>
+		<?php 
 
-	        $cadena.="</tr>";
-	      }
-	    }
-	    catch(PDOException $e) {
-	      echo 'Error: ' . $e->getMessage();
-	    }
 
-	    $cadena.="</tr>";
-	    echo $cadena;
-	    ?>
+		$id_alumno = 1;
 
-			</tbody>
-		</table>
+		$server = "localhost";
+	$usuario = "root";
+	$contra = "";
+	$basedato = "profesorado_cef";
+
+	try {
+	  $con = new PDO('mysql:host='.$server.';dbname='.$basedato.'', "$usuario", "$contra");
+	  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	  $sql = $con->prepare(
+	    'SELECT c.id_materia,c.id_profesor,c.nombre,c.ano,c.formato,c.regimen_c, p.nombre_y_apellido FROM catedras AS c INNER JOIN profesores AS p ON c.id_profesor = p.id_profesor'
+	  );
+
+	  $sql->execute();
+
+	$cadena = "";
+	$cadena.="<tr>";
+	  while( $datos = $sql->fetch(PDO::FETCH_ASSOC)){
+
+
+	  	$cadena.="<th scope='row'>".$datos['id_materia']."</th> 
+	  	 
+	  	 <td>".$datos['nombre']."</td>
+	  	 <td>".$datos['ano']."</td>
+	  	 <td>".$datos['formato']."</td>	
+	  	 <td>".$datos['regimen_c']."</td>
+	  	 <td><button class='btn btn-outline-primary' onclick='inscribir(".$id_alumno.",".$datos['id_materia'].")'>Aqui</button></td>";
+	  	
+
+	$cadena.="</tr>";
+	  }
+	   
+	} catch(PDOException $e) {
+	  echo 'Error: ' . $e->getMessage();
+	}
+
+
+	$cadena.="</tr>";
+	echo $cadena;
+
+
+	 ?>
+
+	</tbody>
+
+	</table>
+</div>
+
+
 	</div>
 
+	</body>
+
+
 	<script type="text/javascript">
-	  function inscribir(id_alumno,id_catedra) {
-	  	if(confirm("Esta seguro que desea inscribirse?")){
+		
 
-	      var parametros = {
-	        "id_alumno": id_alumno,
-	        "id_catedra": id_catedra
-	      };
+	function inscribir(id_alumno,id_catedra)
+	{
+		if(confirm("Esta seguro que desea inscribirse?")){
 
-	      var url = "guardar_inscripcion_de_alumno_a_la_catedra.php";
+	var parametros = {
+	  "id_alumno": id_alumno,
+	  "id_catedra": id_catedra
+	};
 
-	      $.ajax({
-	        type: "GET",
-	        url: url,
-	        data:parametros ,
-	        success: function(data) {
-	        alert(data);
-	        }
-	      });
-	    }
-	  }
+	var url = "guardar_inscripcion_de_alumno_a_la_catedra.php";
+
+	 $.ajax({                        
+	           type: "GET",                 
+	           url: url,                    
+	           data:parametros ,
+
+	            success: function(data)            
+	           {
+	            alert(data);
+	             
+	           }
+	  });
+
+
+
+	
+		}
+
+		}
+
+
+		$(document).ready(function(){
+
+
+
+   $("#btn-menu").click(function () {
+      $("#menu").each(function() {
+        displaying = $(this).css("display");
+        if(displaying == "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","block");
+          });
+        }
+      });
+    });
+  });
+
+
+
 	</script>
+
+
 	<script type="text/javascript"></script>
-</body>
-</html>
+	</html>
